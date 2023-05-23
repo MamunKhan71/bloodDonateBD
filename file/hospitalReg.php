1c124a437d500c379da282b5dcaca44fe9b13b21
Md. Mamun <mkmamun031@gmail.com>
2023-05-24 00:32:16 +0600

    Code Improvements!
    Added Animation for the index page!

--- file/hospitalReg.php
+++ file/hospitalReg.php
@@ -8,19 +8,22 @@ if(isset($_POST['hregister'])){
 	$hcity=$_POST['hcity'];
 	$check_email = mysqli_query($conn, "SELECT hemail FROM hospitals where hemail = '$hemail' ");
 	if(mysqli_num_rows($check_email) > 0){
-    $error= 'ইতোমধ্যেই রেজিস্ট্রেশন হয়েছে, দয়া করে লগিন করুন';
-    header( "location:../register.php?error=".$error );
-}else{
-	$sql = "INSERT INTO hospitals (hname, hemail, hpassword, hphone, hcity)
-	VALUES ('$hname','$hemail', '$hpassword', '$hphone', '$hcity')";
-	if ($conn->query($sql) === TRUE) {
-		$msg = 'রেজিস্ট্রেশন সফল হয়েছে, লগিন করুন';
-		header( "location:../login.php?msg=".$msg );
+		$error= 'ইতোমধ্যেই রেজিস্ট্রেশন হয়েছে, দয়া করে লগিন করুন';
+		header("Location: ../register.php?error=" . urlencode($error));
+		exit();
 	} else {
-		$error = "সমস্যা: " . $sql . "<br>" . $conn->error;
-        header( "location:../register.php?error=".$error );
+		$sql = "INSERT INTO hospitals (hname, hemail, hpassword, hphone, hcity)
+		VALUES ('$hname','$hemail', '$hpassword', '$hphone', '$hcity')";
+		if ($conn->query($sql) === TRUE) {
+			$msg = 'রেজিস্ট্রেশন সফল হয়েছে, লগিন করুন';
+			header("Location: ../login.php?msg=" . urlencode($msg));
+			exit();
+		} else {
+			$error = "সমস্যা: " . $sql . "<br>" . $conn->error;
+			header("Location: ../register.php?error=" . urlencode($error));
+			exit();
+		}
+		$conn->close();
 	}
-	$conn->close();
-}
 }
 ?>
--- index.php
+++ index.php
@@ -6,19 +6,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
-<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-<script src="js/jquery-3.3.1.min.js"></script>
-<meta name="robots" content="index, follow" />
-
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-351688KRBD"></script>
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+  <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <meta name="robots" content="index, follow" />
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-351688KRBD"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
   <style>
     /* Custom styles */
     body {
-
       font-family: Li Ador Noirrit;
-      background-color: #fffff;
+      background-color: #ffffff;
     }
 
     .navbar {
@@ -62,22 +62,23 @@
       color: #fff;
       text-decoration: none;
     }
+
     hr {
-  border: 0;
-  text-align: center;
-  
-  &:before {
-    content: "\2022 \2022 \2022 \2022 \2022 \2022\2022\2022";
-    color: #e2ede9;
-  }
-}
-.gap{
-  padding: 40px;
-}
+      border: 0;
+      text-align: center;
+      &:before {
+        content: "\2022 \2022 \2022 \2022 \2022 \2022\2022\2022";
+        color: #e2ede9;
+      }
+    }
+
+    .gap {
+      padding: 40px;
+    }
   </style>
 </head>
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark" >
+  <nav class="navbar navbar-expand-lg navbar-dark">
     <a class="navbar-brand" href="#">বাংলাদেশ ব্লাড ব্যাংক</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -86,13 +87,13 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="index.php">হোম <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="#home">হোম <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="index.php?#about">আমাদের সম্পর্কে</a>
+          <a class="nav-link" href="#about">আমাদের সম্পর্কে</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="index.php?#contact">যোগাযোগ</a>
+          <a class="nav-link" href="#contact">যোগাযোগ</a>
         </li>
         <li class="nav-item active">
           <a class="nav-link" href="login.php">লগিন<span class="sr-only">(current)</span></a>
@@ -111,7 +112,7 @@
       <li data-target="#demo" data-slide-to="1"></li>
       <li data-target="#demo" data-slide-to="2"></li>
     </ul>
-  
+
     <!-- The slideshow -->
     <div class="carousel-inner">
       <div class="carousel-item active">
@@ -124,7 +125,7 @@
         <img src="image/blog-4.jpg" alt="New York">
       </div>
     </div>
-  
+
     <!-- Left and right controls -->
     <a class="carousel-control-prev" href="#demo" data-slide="prev">
       <span class="carousel-control-prev-icon"></span>
@@ -135,25 +136,25 @@
   </div>
 
   <section class="my-5" id="about">
-  <div class="py-3 d-flex flex-column align-items-center">
-    <h2 class="text-center">আমাদের সম্পর্কে</h2>
-    <hr>
-  </div>
-  <div class="gap">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6 col-md-6 col-12">
-          <img src="image/about1.jpg" class="img-fluid aboutimg">
-        </div>
-        <div class="col-lg-6 col-md-6 col-12 d-flex flex-column justify-content-center">
-          <h1>তুচ্ছ নয় রক্তদান <br> <h3>বাঁচাতে পারে একটি প্রাণ<h3></h1>
-          <p>প্রত্যেক মানুষের দ্বারা সকল কাজ সম্ভব হয় না। এমন হতে পারে যে আপনার অনেক বড় বড় মহৎ কাজের ইচ্ছে আছে, কিন্তু আপনার সামর্থ্য নেই। কিন্তু একটি কাজ যা আমরা সকলেই করতে পারি, আর সেটা হলো রক্তদান।</p>
-          <h3 class="py-3"></h3>
+    <div class="py-3 d-flex flex-column align-items-center">
+      <h2 class="text-center">আমাদের সম্পর্কে</h2>
+      <hr>
+    </div>
+    <div class="gap">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 col-md-6 col-12">
+            <img src="image/about1.jpg" class="img-fluid aboutimg">
+          </div>
+          <div class="col-lg-6 col-md-6 col-12 d-flex flex-column justify-content-center">
+            <h1>তুচ্ছ নয় রক্তদান <br> <h3>বাঁচাতে পারে একটি প্রাণ<h3></h1>
+              <p>প্রত্যেক মানুষের দ্বারা সকল কাজ সম্ভব হয় না। এমন হতে পারে যে আপনার অনেক বড় বড় মহৎ কাজের ইচ্ছে আছে, কিন্তু আপনার সামর্থ্য নেই। কিন্তু একটি কাজ যা আমরা সকলেই করতে পারি, আর সেটা হলো রক্তদান।</p>
+              <h3 class="py-3"></h3>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
 
 
   <section class="my-5" id="contact" style="background-color: red">
@@ -167,10 +168,25 @@
         </div>
       </div>
     </div>
-    
+
   </section>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      // Smooth scrolling for navigation links
+      $('a.nav-link[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+          event.preventDefault();
+          $('html, body').stop().animate({
+            scrollTop: target.offset().top
+          }, 800, 'easeInOutExpo');
+        }
+      });
+    });
+  </script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
